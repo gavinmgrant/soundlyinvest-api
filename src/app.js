f79b3f331c056f8382683065452d16aee5da5633
Gavin Grant <gavingrant@gmail.com>
2020-11-06 16:23:44 -0800

    refactor(app): update morgan and cors code

--- src/app.js
+++ src/app.js
@@ -7,13 +7,15 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test',
+}))
 app.use(helmet())
-app.use(cors())
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/api/', (req, res) => {
     res.send('Hello, world!');
